f4953c77103fc62746f11bb8873e21acf7c67fa7
Kanchana Vivekanandhan <kanch.cc@gmail.com>
2015-12-23 12:48:58 -0800

    Added Armchair Review Manager using Cocoapods.

--- TippingPal/AppDelegate.swift
+++ TippingPal/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Armchair
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Armchair.appID("987257804")
+        //Armchair.userDidSignificantEvent(true)
+        //Armchair.significantEventsUntilPrompt(1)
+        Armchair.debugEnabled(true)
         return true
     }
 
